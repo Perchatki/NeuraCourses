a9322b16a1cf75075c577f2a7b7763f9f309408b
hitin4teen <79216656+hitin4teen@users.noreply.github.com>
2021-03-30 22:10:49 +0300

    debug

--- neural network lol.py
+++ neural network lol.py
@@ -1,5 +1,5 @@
 import numpy
-import scipy
+import scipy.special
 import matplotlib.pyplot
 
 class Neuralnetwork:
@@ -9,6 +9,8 @@ class Neuralnetwork:
         self.hid_n=hidnode #hidden nodes
         self.out_n=outnode #output nodes
         self.l_r=learnrate
+        
+        self.activ_func = lambda x: scipy.special.expit(x)
         
         #генерация матриц слоев
         #wih = weights input-hidden
@@ -20,7 +22,6 @@ class Neuralnetwork:
     def query(self, input_l):
         
         #normalisation(sigmoid)
-        self.activ_func = lambda x: scipy.special.expit(x)
         
         inputs = numpy.array(input_l, ndmin=2).T
         
@@ -42,16 +43,18 @@ class Neuralnetwork:
         #targets - Целевое значение
         targets = numpy.array(target_l, ndmin=2).T
         
-        #outputs out_i = final_inputs out_o = final_outputs
-        self.out_i = numpy.dot(self.who, hid_o)
-        out_o=self.activ_func(out_i)
+        
         
         #hidden hid_i = hidden_inputs hid_o = hidden_outputs
         self.hid_i = numpy.dot(self.wih,inputs)
-        hid_o=self.activ_func(hid_i)
+        hid_o=self.activ_func(self.hid_i)
+        
+        #outputs out_i = final_inputs out_o = final_outputs
+        self.out_i = numpy.dot(self.who, hid_o)
+        out_o=self.activ_func(self.out_i)
         
         out_e = targets - out_o
-        hid_e = targets - hid_o
+        hid_e = numpy.dot(self.who.T,out_e)
         
         #обновление слоев
         self.who = self.l_r * numpy.dot((out_e * out_o * (1-out_o)), numpy.transpose(hid_o))
@@ -64,16 +67,16 @@ lr   = 0.3
 
 nn1 = Neuralnetwork(inpn, hidn, outn, lr)
 
-data_file=open("D:/neural networks/mnist_dataset/train.csv",'r')
+data_file=open("C:/Users/Danii/Desktop/progs/programming/Jupiter/mnist_train_100.csv",'r')
 data_list=data_file.readlines()
 data_file.close()
 
 for record in data_list[0:100]:
     values = record.split(',')
-    inputs = numpy.asfarray(values[1:])
-    targets = numpy.zeros(outn) + 0.01
-    targets[int(values[0])] = 0.99
-    nn1.train(inputs,targets)
+    inputss = numpy.asfarray(values[1:])
+    targetss = numpy.zeros(outn) + 0.01
+    targetss[int(values[0])] = 0.99
+    nn1.train(inputss,targetss)
 
 
 num = 0
